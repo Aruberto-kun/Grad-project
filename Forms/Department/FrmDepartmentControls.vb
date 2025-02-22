30adb8b636f3e62faf6004ef2b1132f487f18341
Aru <103089740+Aruberto-kun@users.noreply.github.com>
2025-02-22 08:56:44 +0800

    Capstone 2

---
+++ Forms/Department/FrmDepartmentControls.vb
@@ -0,0 +1,35 @@
+﻿Public Class FrmDepartmentControls
+    Private Sub TPDepartmentList_Enter(sender As Object, e As EventArgs) Handles TPDepartmentList.Enter
+        ClassDepartment.LoadDepartmentDG(DGDepartment)
+    End Sub
+    Private Sub FrmDepartmentControls_Load(sender As Object, e As EventArgs) Handles Me.Load
+        OpenServerConnection()
+        ClassDepartment.LoadDepartmentDG(DGDepartment)
+    End Sub
+    Private Sub TPDepartmentProfile_Enter(sender As Object, e As EventArgs) Handles TPDepartmentProfile.Enter
+        ClassDepartment.LoadDepartment(CbDepartment)
+        ClassDepartment.LoadDepartmentHead(CbDepartmentHead, CbDepartment)
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+        If CbDepartment.SelectedIndex = -1 Then
+            MsgEmptyField()
+            Exit Sub
+        ElseIf CbDepartmentHead.SelectedIndex = -1 Then
+            MsgEmptyField()
+            Exit Sub
+        End If
+        ClassDepartment.SaveDepartmentHead(DGDepartment)
+    End Sub
+
+    Private Sub CbDepartment_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CbDepartment.SelectedIndexChanged
+        Try
+            If CbDepartment.SelectedIndex = -1 Then
+                Exit Sub
+            End If
+            ClassDepartment.LoadDepartmentHead(CbDepartmentHead, CbDepartment)
+
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
